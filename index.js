ee56d44fd8b99bdf3d86a3ac5e3b494e17e39772
Peterson Chaves <pchaves83@gmail.com>
2017-02-21 22:56:03 -0300

    Initial test

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/',function(req, res){
+	console.log('Recebida a requisicao de teste');
+	res.send('<html><body>Página principal =)</body></html>');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
